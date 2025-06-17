753d08a692df057f913bc2414ca356825b551f36
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2025-06-17 20:15:40 +0700

    update

--- Customer/customer.gd
+++ Customer/customer.gd
@@ -7,7 +7,20 @@ class_name Customer
 @onready var face: Sprite2D = %Face
 @onready var hand_left: Sprite2D = %HandLeft
 @onready var hand_right: Sprite2D = %HandRight
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var item_box: Control = $ItemBox
+@onready var item_img: TextureRect = $ItemBox/TextureRect/ItemImg
+@onready var item_label: Label = $ItemBox/TextureRect/ItemLabel
 
+var request_item: Item
+var request_qty: int
+var current_order_status: int
+
+func init_customer(item:Item, quantity:int) -> void:
+	request_item = item
+	request_qty = quantity
+	current_order_status = quantity
+	show_order_ui()
 
 func set_sprites(data:CustomerData) -> void:
 	body.texture = data.body
@@ -15,3 +28,12 @@ func set_sprites(data:CustomerData) -> void:
 	hand_left.texture = data.hand
 	hand_right.texture = data.hand
 	pass
+	
+func show_order_ui():
+	item_box.show()
+	item_img.texture = request_item.sprite
+	item_label.text = str(request_qty)
+	pass
+	
+func play_move_anim() -> void:
+	animation_player.play('move')
---
+++ autoloads/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+const ITEM_BURGER = preload("res://data/Item_burger.tres")
+const ITEM_COFFEE = preload("res://data/item_coffee.tres")
+
+func get_random_item() -> Item:
+	var items: Array = [ITEM_BURGER, ITEM_COFFEE]
+	return items.pick_random()
+	
